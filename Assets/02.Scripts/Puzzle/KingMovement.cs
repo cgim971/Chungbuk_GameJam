c25920b39bb427605dff30bd497cedd4760279a3
ChanheeKim <cgim971@gmail.com>
2022-08-21 01:37:06 +0900

    모르겟다
    일단 올림

--- Assets/02.Scripts/Puzzle/KingMovement.cs
+++ Assets/02.Scripts/Puzzle/KingMovement.cs
@@ -37,18 +37,19 @@ public class KingMovement : MonoBehaviour
 
     public void FindRoad()
     {
-        int index = 0;
-        foreach (WordController word in wordControllerList)
-        {
-            for (int i = 0; i < word.CircleCollider.Count; i++)
-            {
-                word.CircleCollider[i].GetComponent<CircleInfo>().Number = index++;
-            }
-            index = 0;
-        }
+        //int index = 0;
+        //foreach (WordController word in wordControllerList)
+        //{
+        //    for (int i = 0; i < word.CircleCollider.Count; i++)
+        //    {
+        //        word.CircleCollider[i].GetComponent<CircleInfo>().Number = index++;
+        //    }
+        //    index = 0;
+        //}
 
 
-
+        // 되면 움직임
+        // 안 되면 안 움직임
 
 
     }
