8f64c9e1b787a39837910598f0fc1734f8fd9780
EunSung <82389915+EnSung@users.noreply.github.com>
2022-08-21 02:47:03 +0900

    .
    .

---
+++ Assets/02.Scripts/Eunseongg/DataManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    private static DataManager instance;
+    public static DataManager Instance { get { return instance; } }
+    public UserData data = new UserData();
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void Save()
+    {
+        string json = JsonUtility.ToJson(data);
+        Debug.Log(json);
+        File.WriteAllText(Application.dataPath + "/Resources/Userdata.json",json);
+    }
+
+    public void Load()
+    {
+        TextAsset TA = Resources.Load<TextAsset>("Userdata");
+        data = JsonUtility.FromJson<UserData>(TA.text);
+    }
+}
---
+++ Assets/02.Scripts/Eunseongg/UserData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData
+{
+    public int coin;
+    public bool[] havingSkin = new bool[5];
+    public int currentWearingSkinIndex;
+    public bool[] stageClear;
+
+}
